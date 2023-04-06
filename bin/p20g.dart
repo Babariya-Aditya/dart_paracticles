e05314336da564cc7bf4dbf3cef775446c14289a
Babariya-Aditya <adityababariya43@gmail.com>
2023-04-06 17:03:59 +0530

    hiii

--- bin/p20g.dart
+++ bin/p20g.dart
@@ -3,5 +3,14 @@ import 'dart:io';
 void main(){
   print("enter the number");
   int n=int.parse(stdin.readLineSync()!);
+  int? rev=0;
+  int n1;
+  for(int i=0;i<=n.bitLength;i++){
+    n1=n%10;
+    rev=rev!*10+n1;
+    n=n~/10;
+
+  }
+  print("$rev");
 
 }
